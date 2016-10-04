b4a20e1fe5c9e4c44435371b9536c121ad18f8e2
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-10-04 12:00:29 -0400

    Use method call to set identifier and API key

--- Swift Documentation Sample Application/AppDelegate.swift
+++ Swift Documentation Sample Application/AppDelegate.swift
@@ -7,9 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        let credentials = CMAPICredentials.sharedInstance() as! CMAPICredentials
-        credentials.appIdentifier = "c231a674044045af994fa0256007e1cc"
-        credentials.apiKey = "9d94c776b4834cd6ba0085da71113a68"
+        CMAPICredentials.sharedInstance().setAppIdentifier("c231a674044045af994fa0256007e1cc", andApiKey: "9d94c776b4834cd6ba0085da71113a68")
 
         return true
     }
