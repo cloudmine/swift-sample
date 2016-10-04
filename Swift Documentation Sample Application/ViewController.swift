96713b86079ffe25618cffb387e749168da1e7ba
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-10-04 12:21:36 -0400

    Refactor/cleanup of sample ViewController Code

--- Swift Documentation Sample Application/ViewController.swift
+++ Swift Documentation Sample Application/ViewController.swift
@@ -5,56 +5,42 @@ class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Creating an object
-        let car: CMCar = CMCar();
-        //setting its properties
-        car.year = 2014;
+
+        let car: CMCar = CMCar() //Creating an object
+
+        car.year  = 2014 //setting its properties
         car.model = "Model S"
-        car.make = "Tesla"
+        car.make  = "Tesla"
         
         //saving the object
-        car.save {
-            (response : CMObjectUploadResponse!) -> Void in
+        car.save { (response: CMObjectUploadResponse!) in
             print("[CMSAMPLE] Object was uploaded successfully! %@", response.uploadStatuses)
         }
         
-        CMStore.defaultStore().searchObjects("[\"Tesla\"]", additionalOptions: nil) {
-            (response : CMObjectFetchResponse!) in
-            print("[CMSAMPLE]  JSON: " + response.description)
-        }
-        
-        CMStore.defaultStore().allObjectsWithOptions(nil)
-            {
-            (response : CMObjectFetchResponse!) in
+        CMStore.defaultStore().searchObjects("[\"Tesla\"]", additionalOptions: nil) { (response: CMObjectFetchResponse!) in
             print("[CMSAMPLE] JSON: " + response.description)
         }
         
-        CMStore.defaultStore().allObjectsOfClass(CMCar.self, additionalOptions: nil)
-            {
-                (response : CMObjectFetchResponse!) in
+        CMStore.defaultStore().allObjectsWithOptions(nil) { (response : CMObjectFetchResponse!) in
+            print("[CMSAMPLE] JSON: " + response.description)
+        }
+        
+        CMStore.defaultStore().allObjectsOfClass(CMCar.self, additionalOptions: nil) { (response: CMObjectFetchResponse!) in
                 print("[CMSAMPLE] JSON: " + response.description)
         }
+
         let keys = ["ObjectID","secondObjectID"]
-        CMStore.defaultStore().objectsWithKeys(keys, additionalOptions: nil)
-            {
-                (response : CMObjectFetchResponse!) in
+        CMStore.defaultStore().objectsWithKeys(keys, additionalOptions: nil) { (response: CMObjectFetchResponse!) in
                 print("[CMSAMPLE] JSON: " + response.description)
         }
         
-        CMStore.defaultStore().deleteObject(car, additionalOptions: nil)
-            {
-                (response : CMDeleteResponse!) in
+        CMStore.defaultStore().deleteObject(car, additionalOptions: nil) { (response: CMDeleteResponse!) in
                 print("[CMSAMPLE] JSON: " + response.description)
         }
-        
         
         car.currentLocation = CMGeoPoint(latitude: 30, andLongitude: 30)
-        CMStore.defaultStore().searchObjects("[currentLoc near (30,30)]", additionalOptions: nil)
-            {
-                (response : CMObjectFetchResponse!) in
+        CMStore.defaultStore().searchObjects("[currentLoc near (30,30)]", additionalOptions: nil) { (response: CMObjectFetchResponse!) in
                 print("[CMSAMPLE] JSON: " + response.description)
         }
     }
 }
-
